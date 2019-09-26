03d21eeabaed9f7cf9cf84581fda7029d6672ab2
halle <halle.mills.2022@student.lhschools.org>
2019-09-26 09:50:13 -0400

    first commit

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,57 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //todo declare variables a,b,c,discriminate, axisOfSymmetry,parabolaFacing,x, y
+
+        //todo initialize manually
+
+        //todo do calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialized area
+
+        //todo double check results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
