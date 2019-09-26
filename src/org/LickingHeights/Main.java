1a28157304184e6a38377766cac2ab97e40b86c7
halle <halle.mills.2022@student.lhschools.org>
2019-09-26 10:04:36 -0400

    did calculations for axis of symmetry and discriminant

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,13 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         //todo declare variables a,b,c,discriminate, axisOfSymmetry,parabolaFacing,x, y
-        double a,b,c,discriminate,axisOfSymmetry,x,y;
+        double a,b,c,discriminant,axisOfSymmetry,x,y;
         String parabolaFacing;
         //todo initialize manually
         a=5;
         b=3;
         c=1;
         //todo do calculations
+        axisOfSymmetry = -b/(2*a);
+        discriminant = (b*b)-4*(a*c);
 
         //todo print results
 
